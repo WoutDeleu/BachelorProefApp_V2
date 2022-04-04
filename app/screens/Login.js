b79ca65a7998b9554c3b841f4f083ff995a6d69b
WoutDeleu <80529253+WoutDeleu@users.noreply.github.com>
2022-04-04 20:44:56 +0200

    Login poging om het werkende te krijgen (copy from Toon)

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,73 +1,129 @@
 import React from 'react';
-import { StyleSheet, Icon, Text, View, TextInput, TouchableOpacity,Image, Dimensions } from 'react-native';
-
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { Root, Popup } from 'popup-ui';
+import { useState } from "react";
+import axios from "axios";
+import jwt_decode from 'jwt-decode';
+import qs from "qs";
+import useAuth from '../hooks/useAuth';
 
 function Login() {
     const win = Dimensions.get('window');
 
-    this.state ={
-        email:"",
-        password:"",
-        icon:"eye-off",
-        passwordVis: true
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { auth, setAuth } = useAuth();
+    const [errMsg, setErrMsg] = useState('');
+    const validateEmail = (text) => {
+        console.log(text);
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+        if (reg.test(text) === false) {
+            console.log("Email is Not Correct");
+            setEmail(text);
+            return false;
+        }
+        else {
+            setEmail(text);
+            console.log("Email is Correct");
+        }
     }
 
-    this.setEmail = function (param) {
-        this.state.email = param;
-    }
-    this.setPasw = function (param) {
-        this.state.password = param;
+
+    const logInCheck = async (e) => {
+        e.preventDefault();
+        const data = qs.stringify({email, password});
+        const config = {
+            method: 'post',
+            url: 'http://192.168.20.181:8081/authentication/login',
+            headers: {
+                'Content-Type': "application/x-www-form-urlencoded"
+            },
+            data: data
+        };
+        axios(config).then(function(res){
+            const decoded = jwt_decode(res.data.access_token);
+            const roles = decoded.roles;
+            setAuth({email,password, roles});
+
+            localStorage.setItem("access_token", JSON.stringify(res.data.access_token));
+            localStorage.setItem("refresh_token", JSON.stringify(res.data.refresh_token));
+            navigate(from, {replace: true});
+        }).catch(function (error) {
+            console.log(error.response?.status)
+            if (!error?.response) {
+                setErrMsg('No Server Response');
+            } else if (error.response?.status ===401){
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed')
+            }
+            errRef.current.focus();
+        });
+
+
+        if(true /*Check if pasword/username is correct*/) {
+            Popup.show({
+                type: 'Danger',
+                title: 'Password/Username are incorrect',
+                button: true,
+                textBody: 'Your password and/or username are incorrect. Please try again ',
+                buttontext: 'Ok',
+                callback: () => Popup.hide()
+            })
+        }
+        else {}
+        console.log('password = ' + password.toString());
+        console.log('email = ' + email.toString());
+
     }
 
-    function changeIcon() {
-        this.setState(prevState=>({
-           icon: prevState.icon === 'eye' ? 'eye-off' : 'eye',
-           password: !prevState.passwordVis
-        }));
-    }
     return(
-        <View style={styles.container}>
-            <Image
-                source={require('../../assets/pics/KUL_FullLogo.png')}
-                style={{
-                    width: win.width/2,
-                    height: (win.width/2646)*670,
-                    resizeMode: "contain",
-                    alignSelf: "center",
-                    borderWidth: 1,
-                    backgroundColor: "transparent"
-                    //borderRadius: 20,
-                }}
-            />
+        <Root>
+            <View style={styles.container}>
+                <Image
+                    source={require('../../assets/pics/KUL_FullLogo.png')}
+                    style={{
+                        width: win.width/2,
+                        height: (win.width/2646)*670,
+                        resizeMode: "contain",
+                        alignSelf: "center",
+                        borderWidth: 1,
+                        backgroundColor: "transparent"
+                        //borderRadius: 20,
+                    }}
+                />
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Email..."
-                    placeholderTextColor="#212521"
-                    onChangeText={text => this.setEmail({email:text})}/>
+                <View style={styles.inputView}>
+                    <TextInput
+                        autoComplete={email}
+                        type={email}
+                        style={styles.inputText}
+                        placeholder="Email..."
+                        placeholderTextColor="#212521"
+                        onChangeText={text => validateEmail(text)}
+                    />
+                </View>
+                <View style={styles.inputView} >
+                    <TextInput
+                        secureTextEntry
+                        style={styles.inputText}
+                        placeholder="Password..."
+                        placeholderTextColor="#212521"
+                        onChangeText={text => setPassword(text)}/>
+                </View>
+                <TouchableOpacity>
+                    <Text style={styles.forgot}>Forgot Password?</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.loginBtn} onPress={logInCheck}>
+                    <Text style={styles.loginText}>LOGIN</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={{color: "black"}}>
+                        Register as a company?
+                    </Text>
+                </TouchableOpacity>
             </View>
-            <View style={styles.inputView} >
-                <TextInput
-                    secureTextEntry
-                    style={styles.inputText}
-                    placeholder="Password..."
-                    placeholderTextColor="#212521"
-                    onChangeText={text => this.setPasw({password:text})}/>
-            </View>
-            <TouchableOpacity>
-                <Text style={styles.forgot}>Forgot Password?</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.loginBtn}>
-                <Text style={styles.loginText}>LOGIN</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text style={styles.loginText}>Signup</Text>
-            </TouchableOpacity>
-
-
-        </View>
+        </Root>
     )
 }
 
@@ -77,8 +133,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        borderTopLeftRadius:25,
-        borderTopRightRadius:25,
+        //borderTopLeftRadius:25,
+        //borderTopRightRadius:25,
     },
     logo:{
         fontWeight:"bold",
